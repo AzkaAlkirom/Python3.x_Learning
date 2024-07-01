9ab72b35afa9f6c3f9c0ca302d2f5692f19c5165
Azka Ajaa <50364511+AzkaAlkirom@users.noreply.github.com>
2024-07-02 02:20:06 +0700

    Konsisten 0.9

---
+++ 9. For Loop (Perulangan)/main.py
@@ -0,0 +1,37 @@
+# Belajar Loop (Perulangan)
+
+
+
+# Ini looping pakai list
+print("\n"+" Loop Pake List ".center(32,"="))
+
+angka2_list = [0,2,4,6,8,10] # ini list nya 
+
+for i in angka2_list: #ini loop nya
+    print(f"Ini angka {i}")
+
+
+# ini looping pakai range
+
+print("\n"+" Loop Pake Range ".center(32,"="))
+angka2_range = range(1,8) #ini range 1-5
+
+for i in angka2_range: #ini loop nya
+    print(f"Ini angka perulangan ke --> {i}")
+    # print("Aku Kuat")
+
+
+# ini looping with a string 
+
+print("\n"+" Loop Pake string ".center(32,"="))
+angka2_range = "Semangat Bro" #ini string nya
+
+for i in angka2_range: #ini loop nya
+    print(i) #outputnya akan menurun
+
+"""
+        Oke itu dia latihan loop,
+    abis ini bakal latihan while loop
+simple but jangan di sepelekan hehe, penting nih
+                    byee
+"""
