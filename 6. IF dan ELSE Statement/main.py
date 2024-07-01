d708bf22d8947e6feeb55cd126cdef546ae9fbfa
Azka Ajaa <50364511+AzkaAlkirom@users.noreply.github.com>
2024-07-02 02:17:25 +0700

    Konsisten 0.6

---
+++ 6. IF dan ELSE Statement/main.py
@@ -0,0 +1,37 @@
+# IF dan ELSE Statement
+
+'''
+    1. If nya       : 
+    2. Kondisi nya  : 
+    3. Aksinya      : 
+'''
+# Program if kondisi : aksi
+
+print("\n"+7*"="+" IF STATEMENT "+"="*7)
+
+nama = input("Siapa Nama Anda : ")
+
+
+## 1. Program if inline
+
+if nama == "ucup" : print("Kamu Anak Hebat")
+print(f"Terima kasih {nama}")
+print("------ Akhir Program -----")
+
+# 2. Program if indentation
+
+if nama == "azka" :
+    print(f"\nHaii Azka, Kamu baik kan?")
+    print("gapapaa kok Azka, ini cuma cobaan sesaat")
+else :
+    print(f"Maaf ya, kamu bukan Azka ")
+    print(f"Ku cuma Mau Azka:) bukan {nama}")
+
+pamit = " See you ".center(35,"=") 
+print(pamit)
+
+'''
+        Simple manteman, tapi harus di inget inget
+    kalo ini bukan pasal If atau Else, true or false bro
+        ini pasal masa depan yang diperjuangkan
+'''
