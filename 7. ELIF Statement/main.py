a9a96fdfb5866bc1156b8d7c84d79cc2c62b6e4c
Azka Ajaa <50364511+AzkaAlkirom@users.noreply.github.com>
2024-07-02 02:18:09 +0700

    Konsisten 0.7

---
+++ 7. ELIF Statement/main.py
@@ -0,0 +1,39 @@
+# elif + else if statement
+
+''' Contoh : 
+if nama == "azka" :
+    print("Hello bro!!")
+elif nama == "Yanto" : 
+    print("yuhu yantooo kun")
+elif nama "ucup" :
+    print("yuhu yantooo kun")
+else:
+    print("Skip males")
+'''
+# jadi kekgitu, elif ini bisa kita kasih sebanyak apapun
+# ga ada batasan nya buat yang mau bibik elif
+
+# Lets strart bro
+
+print("\n"+7*"="+" Elif Statement "+"="*7)
+nama = input("""Nama Kamu siapa dulu nih?
+""")
+if nama == "azka" :
+    print(f"Haloo Broo!! gimana {nama}?")
+    print("Aman kan? Pasti bisa lewatin kok, kalemmm!!")
+elif nama == "imam" :
+    print(f"Haloo Broo!! {nama}, suwun ya wes ngancani Azka")
+elif nama == "abah" :
+    print("terima kasih udah lahirkan anak yang kuat")
+else:
+    print("Maaf, ini bukan buat kamu ")
+
+print("\n"+7*"="+" See You "+"="*7)
+
+'''
+Jadi begitu ya kawan kawan hehehe..
+maaf kalo coding nya agak galauu nihh hehehe
+jadi elif itu bisa di pakai berkali kali sebelum else
+bisa banyak kondisi juga, elif itu termasuk jawaban true 
+yang didalam false hehehe.. SEMANGATTT!! 
+'''
